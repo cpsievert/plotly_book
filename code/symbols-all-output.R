4a3fea8369fdc943659f5be431b222efa2ef2d93
Carson Sievert <cpsievert1@gmail.com>
2019-03-31 21:36:08 -0500

    Build docs from scratch; moar small fixes

---
+++ code/symbols-all-output.R
@@ -0,0 +1,5 @@
+if (knitr::is_html_output()) {
+  source("code/symbols-all.R")
+} else {
+  knitr::include_graphics("images/symbols-all.png")
+}
---
+++ code/trellis-txhousing-plotly-output.R
@@ -0,0 +1,5 @@
+if (knitr::is_html_output()) {
+  source("code/trellis-txhousing-plotly.R")
+} else  {
+  knitr::include_graphics("images/trellis-txhousing-plotly.png")
+}
