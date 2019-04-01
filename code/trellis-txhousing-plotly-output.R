5b04d0804d5c267ff417f9501b182d5f4a6597d9
Carson Sievert <cpsievert1@gmail.com>
2019-04-01 13:41:48 -0500

    fix some html plots not appearing

--- code/trellis-txhousing-plotly-output.R
+++ code/trellis-txhousing-plotly-output.R
@@ -1,5 +1,23 @@
 if (knitr::is_html_output()) {
-  source("code/trellis-txhousing-plotly.R")
+  txsmall %>%
+    group_by(city) %>%
+    do(
+      p = highlight_key(., ~year, group = "txhousing-trellis") %>%
+        plot_ly(showlegend = FALSE) %>%
+        group_by(year) %>%
+        add_lines(
+          x = ~month, y = ~median, text = ~year,
+          hoverinfo = "text"
+        ) %>%
+        add_annotations(
+          text = ~unique(city),
+          x = 0.5, y = 1,
+          xref = "paper", yref = "paper",
+          xanchor = "center", yanchor = "bottom",
+          showarrow = FALSE
+        )
+    ) %>%
+    subplot(nrows = 2, margin = 0.05, shareY = TRUE, shareX = TRUE, titleY = FALSE)
 } else  {
   knitr::include_graphics("images/trellis-txhousing-plotly.png")
 }
