71cca6a0877e1d9429f4557bd4aa3706d1b953d2
Carson <cpsievert1@gmail.com>
2019-10-02 16:24:01 -0500

    try a more custom GeoJSON file

--- data-raw/us-states.R
+++ data-raw/us-states.R
@@ -1,4 +1,29 @@
-download.file(
-  "https://raw.githubusercontent.com/PublicaMundi/MappingAPI/master/data/geojson/us-states.json",
-  "data-raw/us-states.json"
+library(albersusa)
+library(geojsonio)
+
+sf::st_write(
+  usa_sf("laea"),
+  dsn = "data-raw/us-states.json",
+  driver = "GeoJSON",
+  delete_dsn = TRUE
 )
+
+us <- geojson_read("data-raw/us-states.json")
+us$features <- Map(function(x, y) {
+  x$id <- y; x
+}, us$features, usa_sf("laea")$name)
+geojson_write(us, file = "us-states.json")
+
+
+
+# Make sure there is a top-level id for each feature
+# as plotly.js will want us to link the location attribute
+# to this id. TODO: make this easier for others!
+# https://github.com/plotly/plotly.js/issues/4154#issuecomment-526198848
+us_list <- usa_sf("laea") %>%
+  sf_geojson() %>%
+  geojson_list()
+
+
+
+geojson_write(us_list, file = "data-raw/us-states.json")
