c59d5633096a8cdddc04fa881a33b1426852d355
Carson <cpsievert1@gmail.com>
2019-10-02 16:57:48 -0500

    forgot to copy over to site

---
+++ docs/data-raw/us-states.R
@@ -0,0 +1,27 @@
+# Generate a GeoJSON file of US state boundaries
+
+library(rnaturalearth)
+library(sf)
+library(geojsonio)
+
+usa <- ne_states(iso_a2 = "US", returnclass = "sf")
+usa <- usa %>%
+  select(name) %>%
+  filter(!name %in% c("Alaska", "Hawaii"))
+
+st_write(
+  usa,
+  dsn = "data-raw/us-states.json",
+  driver = "GeoJSON",
+  delete_dsn = TRUE
+)
+
+# plotly.js is very insistent on there being a top-level
+# id that is linked to the location attribute
+# https://github.com/plotly/plotly.js/issues/4154#issuecomment-526198848
+# TODO: make this easier!!
+us <- geojson_read("data-raw/us-states.json")
+us$features <- Map(function(x, y) {
+  x$id <- y; x
+}, us$features, as.character(usa$name))
+jsonlite::write_json(us, path = "data-raw/us-states.json", auto_unbox = TRUE)
