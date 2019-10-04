81776e8bb7662ecbeaae1f587eb36fc3bc179b58
Carson <cpsievert1@gmail.com>
2019-10-04 17:16:21 -0500

    Final proofread before submitting to CRC

--- code/trellis-txhousing-plotly.R
+++ code/trellis-txhousing-plotly.R
@@ -1,19 +1,22 @@
 txsmall %>%
   group_by(city) %>%
   do(
-    p = highlight_key(., ~year, group = "txhousing-trellis") %>% 
-      plot_ly(showlegend = FALSE) %>% 
+    p = highlight_key(., ~year, group = "txhousing-trellis") %>%
+      plot_ly(showlegend = FALSE) %>%
       group_by(year) %>%
       add_lines(
-        x = ~month, y = ~median, text = ~year, 
+        x = ~month, y = ~median, text = ~year,
         hoverinfo = "text"
       ) %>%
       add_annotations(
-        text = ~unique(city), 
-        x = 0.5, y = 1, 
-        xref = "paper", yref = "paper", 
-        xanchor = "center", yanchor = "bottom", 
+        text = ~unique(city),
+        x = 0.5, y = 1,
+        xref = "paper", yref = "paper",
+        xanchor = "center", yanchor = "bottom",
         showarrow = FALSE
       )
   ) %>%
-  subplot(nrows = 2, margin = 0.05, shareY = TRUE, shareX = TRUE, titleY = FALSE)
+  subplot(
+    nrows = 2, margin = 0.05,
+    shareY = TRUE, shareX = TRUE, titleY = FALSE
+  )
